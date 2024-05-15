19a8ec5d2428ab290d614da04ba8e1bf09c637d5
surya <107686299+surya663@users.noreply.github.com>
2024-05-15 22:14:08 +0530

    function to show the guessed letters so far

--- hangman.py
+++ hangman.py
@@ -11,14 +11,22 @@ def get_random_word():
 def get_player_input(guessed_letters):
     while True:
         player_input = input("Guess a letter: ").lower()
-        if (player_input not in guessed_letters) and (
-            player_input in string.ascii_lowercase
-        ):
+        if (player_input not in guessed_letters) and (len(player_input) == 1):
             return player_input
 
 
 def join_guessed_letters(guessed_letters):
     return " ".join(sorted(guessed_letters))
+
+
+def show_guessed_word(rand_word, guessed_letters):
+    current_letters = []
+    for letter in rand_word:
+        if letter in guessed_letters:
+            current_letters.append(letter)
+        else:
+            current_letters.append("-")
+    return " ".join(current_letters)
 
 
 def hangman():
