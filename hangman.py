f1cd31338895bcd2b5f20c11a04229652d5dd91f
clean <55951183+clean203@users.noreply.github.com>
2024-05-15 21:05:33 +0530

    getting random words and player input

---
+++ hangman.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+
+def get_random_word():
+    with open("words.txt", "r") as words:
+        word_list = words.readlines()
+    return random.choice(word_list).strip()
+
+
+def get_player_input(guessed_letters):
+    while True:
+        player_input = input("Guess a letter: ").lower()
+        if (player_input not in guessed_letters) and (
+            player_input in string.ascii_lowercase
+        ):
+            return player_input
+
+
+def hangman():
+    rand_word = get_random_word()
+
+
+hangman()
